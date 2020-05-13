c5028c981ad22af001e5ac7196c8ff015fd0df9a
liwenfeng <liwenfeng@ava.com.cn>
2020-05-13 19:05:20 +0800

    完成屏幕编码成h264

--- screenrecord/src/main/java/com/wenfengtou/screenrecord/ScreenRecordActivity.java
+++ screenrecord/src/main/java/com/wenfengtou/screenrecord/ScreenRecordActivity.java
@@ -1,15 +1,19 @@
 package com.wenfengtou.screenrecord;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.AudioFormat;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.yanzhenjie.permission.Action;
 import com.yanzhenjie.permission.AndPermission;
 import com.yanzhenjie.permission.runtime.Permission;
 
-import java.security.Permissions;
 import java.util.List;
 
 public class ScreenRecordActivity extends AppCompatActivity {
@@ -17,6 +21,12 @@ public class ScreenRecordActivity extends AppCompatActivity {
     private MicRecorder mMicRecorder;
     private AudioEncoder mAudioEncoder;
     private AudioConfig mAudioConfig;
+
+    private ScreenRecorder mScreenRecorder;
+    private static final int REQUEST_SCREEN_RECORD = 1;
+
+    private MediaProjectionManager mMediaProjectionManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,12 +38,18 @@ public class ScreenRecordActivity extends AppCompatActivity {
                 .onGranted(new Action<List<String>>() {
                     @Override
                     public void onAction(List<String> data) {
+                        /*
                         mAudioConfig = new AudioConfig(44100, AudioFormat.ENCODING_PCM_16BIT, AudioFormat.CHANNEL_IN_MONO);
                         mMicRecorder = new MicRecorder(mAudioConfig);
                         mAudioEncoder = new AudioEncoder(mAudioConfig);
                         mAudioEncoder.start();
                         mMicRecorder.addCallback(mAudioEncoder);
                         mMicRecorder.start();
+                         */
+                        mScreenRecorder = new ScreenRecorder(ScreenRecordActivity.this);
+                        mMediaProjectionManager = mScreenRecorder.getMediaProjectionManager();
+                        Intent intent = mMediaProjectionManager.createScreenCaptureIntent();
+                        startActivityForResult(intent, REQUEST_SCREEN_RECORD);
                     }
                 })
                 .onDenied(new Action<List<String>>() {
@@ -46,9 +62,26 @@ public class ScreenRecordActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_SCREEN_RECORD) {
+            mScreenRecorder.start(resultCode, data);
+            moveTaskToBack(true);
+        }
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
-        mAudioEncoder.release();
-        mMicRecorder.release();
+        if (mAudioEncoder != null) {
+            mAudioEncoder.release();
+        }
+        if (mMicRecorder != null) {
+            mMicRecorder.release();
+        }
+
+        if (mScreenRecorder != null) {
+            mScreenRecorder.release();
+        }
     }
 }
---
+++ screenrecord/src/main/java/com/wenfengtou/screenrecord/ScreenRecorder.java
@@ -0,0 +1,99 @@
+package com.wenfengtou.screenrecord;
+
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.media.MediaCodec;
+import android.media.MediaCodecInfo;
+import android.media.MediaFormat;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
+import android.os.Environment;
+import android.os.Handler;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+
+import com.wenfengtou.commonutil.FileUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+public class ScreenRecorder {
+
+    private MediaProjectionManager mMediaProjectionManager;
+    private MediaCodec mMediaCodec;
+    private Surface mSurface;
+    private Thread mScreenThread;
+    private int mTimeoutUs = 100;
+    private boolean mIsStart = false;
+    private String mScreenPath = Environment.getExternalStorageDirectory() + File.separator + "ScreenRecord.h264";
+
+    public ScreenRecorder(Context context) {
+        init(context);
+    }
+    private void init(Context context) {
+        mMediaProjectionManager = (MediaProjectionManager) context.getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+        initMediaCodec();
+    }
+
+    private void initMediaCodec() {
+        try {
+            mMediaCodec = MediaCodec.createEncoderByType(MediaFormat.MIMETYPE_VIDEO_AVC);
+            MediaFormat mediaFormat = MediaFormat.createVideoFormat(MediaFormat.MIMETYPE_VIDEO_AVC, 1920, 1080);
+            mediaFormat.setInteger(MediaFormat.KEY_BIT_RATE, 1024 * 1024);
+            mediaFormat.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);
+            mediaFormat.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 1);
+            mediaFormat.setInteger(MediaFormat.KEY_FRAME_RATE, 30);
+            mMediaCodec.configure(mediaFormat, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
+            mSurface = mMediaCodec.createInputSurface();
+            mMediaCodec.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public MediaProjectionManager getMediaProjectionManager() {
+        return mMediaProjectionManager;
+    }
+
+    public void start(int resultCode, Intent data) {
+        new File(mScreenPath).delete();
+        MediaProjection mediaProjection = mMediaProjectionManager.getMediaProjection(resultCode, data);
+        mediaProjection.registerCallback(new MediaProjection.Callback() {
+            @Override
+            public void onStop() {
+                super.onStop();
+            }
+        }, new Handler());
+        VirtualDisplay virtualDisplay = mediaProjection.createVirtualDisplay("wenfeng-display", 1920, 1080, 1 /*dpi*/,
+                DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC,
+                null /*surface*/, null, null);
+        virtualDisplay.setSurface(mSurface);
+        mScreenThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (mIsStart && (!Thread.interrupted())) {
+                    MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
+                    int outputIndex = mMediaCodec.dequeueOutputBuffer(bufferInfo, mTimeoutUs);
+                    while (outputIndex >= 0) {
+                        ByteBuffer byteBuffer = mMediaCodec.getOutputBuffer(outputIndex);
+                        byteBuffer.position(bufferInfo.offset);
+                        byteBuffer.limit(bufferInfo.offset + bufferInfo.size);
+                        byte[] data = new byte[bufferInfo.size];
+                        byteBuffer.get(data);
+                        FileUtil.writeFile(data, mScreenPath);
+                        mMediaCodec.releaseOutputBuffer(outputIndex, false);
+                        outputIndex = mMediaCodec.dequeueOutputBuffer(bufferInfo, mTimeoutUs);
+                    }
+                }
+            }
+        });
+        mIsStart = true;
+        mScreenThread.start();
+    }
+
+    public void release() {
+        mIsStart = false;
+    }
+}
